79d8df106ebe74238806c2f9c52241e9c60a85eb
Erick Jhoel Toledo <toledoerick1999@gmail.com>
2020-02-26 16:51:12 +0100

    Nuevo intento impresora zebra

--- java-version/app/src/main/java/com/jeluchu/zebraprinter/MainActivity.java
+++ java-version/app/src/main/java/com/jeluchu/zebraprinter/MainActivity.java
@@ -2,22 +2,33 @@ package com.jeluchu.zebraprinter;
 
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import com.zebra.android.comm.BluetoothPrinterConnection;
 import com.zebra.android.comm.ZebraPrinterConnectionException;
+import com.zebra.android.printer.GraphicsUtil;
 import com.zebra.android.printer.PrinterLanguage;
 import com.zebra.android.printer.ZebraPrinter;
 import com.zebra.android.printer.ZebraPrinterFactory;
 
+
 public class MainActivity extends AppCompatActivity {
 
     BluetoothPrinterConnection printerConnection;
+    private GraphicsUtil graphicsUtil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*TiffConverter.ConverterOptions options = new TiffConverter.ConverterOptions();
+        options.throwExceptions = false; //Set to true if you want use java exception mechanism;
+        options.availableMemory = 128 * 1024 * 1024; //Available 128Mb for work;
+        options.compressionScheme = CompressionScheme.LZW; //compression scheme for tiff
+        options.appendTiff = false;//If set to true - will be created one more tiff directory, otherwise file will be overwritten
+        TiffConverter.convertToTiff(getResources().getDrawable(R.drawable.ticket).toString(), getFilesDir()+"/out.tif", options, progressListener);*/
 
         findViewById(R.id.btnPrint).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,8 +37,14 @@ public class MainActivity extends AppCompatActivity {
                 catch (Exception ex) { ex.printStackTrace(); }
             }
         });
-
     }
+
+    /*IProgressListener progressListener = new IProgressListener() {
+        @Override
+        public void reportProgress(long processedPixels, long totalPixels) {
+            Log.v("Progress reporter", String.format("Processed %d pixels from %d", processedPixels, totalPixels));
+        }
+    };*/
 
     public void connect() {
 
@@ -41,8 +58,40 @@ public class MainActivity extends AppCompatActivity {
 
         if (printerConnection.isConnected()) {
             try {
+
                 printer = ZebraPrinterFactory.getInstance(PrinterLanguage.ZPL, printerConnection);
-                printer.getGraphicsUtil().printImage(BitmapFactory.decodeResource(this.getResources(), R.drawable.ticket), 0, 0, 550, 2030, false);
+               // printer.getGraphicsUtil().printImage(BitmapFactory.decodeResource(this.getResources(), R.drawable.ticket), 0, 0, 550, 2030, false);
+                String textito ="^XA" +
+                        "^FO220,115^FD1000 Shipping Lane^FS" +
+                        "^FO220,155^FDShelbyville TN 38102^FS" +
+                        "^FO220,195^FDUnited States (USA)^FS" +
+                        "^FO50,250^GB700,1,3^FS" +
+                        "^FX Second section with recipient address and permit information." +
+                        "^CFA,30" +
+                        "^FO50,300^FDJohn Doe^FS" +
+                        "^FO50,340^FD100 Main Street^FS" +
+                        "^FO50,380^FDSpringfield TN 39021^FS" +
+                        "^FO50,420^FDUnited States (USA)^FS" +
+                        "^CFA,15" +
+                        "^FO600,300^GB150,150,3^FS" +
+                        "^FO638,340^FDPermit^FS" +
+                        "^FO638,390^FD123456^FS" +
+                        "^FO50,500^GB700,1,3^FS" +
+                        "^FX Third section with barcode." +
+                        "^BY5,2,270" +
+                        "^FO100,550^BC^FD12345678^FS" +
+                        "^FX Fourth section (the two boxes on the bottom)." +
+                        "^FO50,900^GB700,250,3^FS" +
+                        "^FO400,900^GB1,250,3^FS" +
+                        "^CF0,40" +
+                        "^FO100,960^FDCtr. X34B-1^FS" +
+                        "^FO100,1010^FDREF1 F00B47^FS" +
+                        "^FO100,1060^FDREF2 BL4H8^FS" +
+                        "^CF0,190" +
+                        "^FO470,955^FDCA^FS" +
+                        "^XZ" ;
+
+                printer.getGraphicsUtil().printImage(textito,550, 2030);
             } catch (ZebraPrinterConnectionException e) { disconnect(); }
         }
 
---
+++ java-version/app/src/main/java/com/jeluchu/zebraprinter/pruebaKotlinErick.kt
@@ -0,0 +1,117 @@
+package com.jeluchu.zebraprinter
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.util.Log
+import android.widget.ListView
+import android.content.IntentFilter
+import com.zebra.sdk.comm.BluetoothConnection
+import com.zebra.sdk.comm.ConnectionException
+import com.zebra.sdk.printer.PrinterLanguage
+import com.zebra.sdk.printer.ZebraPrinter
+import com.zebra.sdk.printer.ZebraPrinterFactory
+import java.io.IOException
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var listView: ListView
+    private val mDeviceList = ArrayList<String>()
+    private var mBluetoothAdapter: BluetoothAdapter? = null
+    private var bluetoothDeviceAddress = mutableListOf<String>()
+    val content = "^XA\n" +
+            "^FX Second section with recipient address and permit information.\n" +
+            "^CFA,30\n" +
+            "^FO50,200^FDJohn Doe^FS\n" +
+            "^FO50,340^FD100 Main Street^FS\n" +
+            "^FO50,380^FDSpringfield TN 39021^FS\n" +
+            "^FO50,420^FDUnited States (USA)^FS\n" +
+            "^CFA,15\n" +
+            "^FO600,300^GB150,150,3^FS\n" +
+            "^FO638,340^FDPermit^FS\n" +
+            "^FO638,390^FD123456^FS\n" +
+            "^XZ"
+
+    private val mReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val action = intent?.action
+            if (BluetoothDevice.ACTION_FOUND == action) {
+                val device = intent
+                        .getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                mDeviceList.add(device.name + "\n" + device.address)
+                bluetoothDeviceAddress.add(device.address)
+                Log.i("BT", device.name + "\n" + device.address)
+                listView.adapter = ArrayAdapter(
+                        context,
+                        android.R.layout.simple_list_item_1, mDeviceList
+                )
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        listView = findViewById(R.id.listView)
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        mBluetoothAdapter?.startDiscovery()
+
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(mReceiver, filter)
+
+        listView.setOnItemClickListener { _, _, position, _ ->
+            print(bDeviceAddress = bluetoothDeviceAddress[position], content = content)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(mReceiver)
+    }
+
+    private fun print(bDeviceAddress: String, content: String): ZebraPrinter? {
+        var printer: ZebraPrinter? = null
+        val printerConnection = BluetoothConnection(bDeviceAddress)
+        try {
+            printerConnection.open()
+            if (printer == null) {
+                printer = ZebraPrinterFactory.getInstance(PrinterLanguage.CPCL, printerConnection)
+            }
+            sendToPrint(printer!!, content)
+            printerConnection.close()
+        } catch (e: ConnectionException) {
+            Log.d("ERROR - ", e.message)
+        } finally {
+
+        }
+        return printer
+    }
+
+    private fun sendToPrint(printer: ZebraPrinter, content: String) {
+        try {
+            val filepath = getFileStreamPath("TEMP.LBL")
+            createFile("TEMP.LBL", content)
+            printer.sendFileContents(filepath.absolutePath)
+        } catch (e1: ConnectionException) {
+            Log.d("ERROR - ", "Error sending file to printer")
+        } catch (e: IOException) {
+            Log.d("ERROR - ", "Error creating file")
+        }
+
+    }
+
+    @Throws(IOException::class)
+    private fun createFile(fileName: String, content: String) {
+        val os = this.openFileOutput(fileName, Context.MODE_APPEND)
+        os.write(content.toByteArray())
+        os.flush()
+        os.close()
+    }
+}
